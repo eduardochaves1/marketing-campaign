273473aafaec9b520f94489ad6b8a5d0c4b00e90
Eduardo Chaves <henriqueduardo2002@gmail.com>
2023-04-06 00:12:47 -0300

    STRUCTURAL modifications

---
+++ pages/Modelo_Preditivo.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title='Modelo Preditivo', page_icon='🤖', layout='wide')
+st.write('''
+<style>
+  section.main > div {max-width:75em}
+</style>
+''', unsafe_allow_html=True)
